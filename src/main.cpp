5eb9765c730543024373007ed7eaf32d33384dff
Théo Takla <9513815+TrakJohnson@users.noreply.github.com>
2021-12-25 22:20:31 +0100

    Transfer execution from test to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cmath>
+#include "calc/matrix.h"
+#include "calc/heat.h"
+#include "utils/file.h"
 
 using namespace std;
 
-int main() {  
+void question2() {
+  HeatSystem1D sys([]([[maybe_unused]] double x) -> double { return 1.; },
+		   [](double x) -> double {
+		     return 0.5 + sin(2 * M_PI * x) - 0.5 * cos(2 * M_PI * x);
+		   },
+		   // TODO le choix de dt et dx est motivé dans le README
+		   0., .5, 0.005,  // t
+		   0., 1., 0.1,    // x
+		   true, 0., 0.);  // conditions au bord
+  Matrix r = sys.solve_explicit();
+  writeMatrixToFile(r, sys, "euler_explicite.txt");
+}
+
+int main() {
+  question2();
   return EXIT_SUCCESS;
 }
--- tests/test_heat.cpp
+++ tests/test_heat.cpp
@@ -6,15 +6,5 @@
 using namespace std;
 
 TEST_CASE("example") {
-  HeatSystem1D sys([]([[maybe_unused]] double x) -> double { return 1.; },
-		   [](double x) -> double {
-		     return 0.5 + sin(2 * M_PI * x) - 0.5 * cos(2 * M_PI * x);
-		   },
-		   // TODO le choix de dt et dx est motivé dans le README
-		   0., 2., 0.005,  // t
-		   0., 1., 0.1,    // x
-		   true, 0., 0.);  // conditions
-  Matrix r = sys.solve_explicit();
-  // cout << r << endl;
-  writeMatrixToFile(r, "test.txt");
+
 }
